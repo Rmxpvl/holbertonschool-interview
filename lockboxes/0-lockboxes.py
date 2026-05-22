8bd547567967a5c9dc865bc033b75205736a53ae
Rmxpvl <remy.pinville@gmail.com>
2026-05-22 18:10:37 +0200

    Refactor canUnlockAll function for clarity and safety checks

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -1,12 +1,18 @@
+#!/usr/bin/python3
 def canUnlockAll(boxes):
+    """Return True if all boxes can be opened starting from box 0."""
     n = len(boxes)
+    if n == 0:
+        return True
+
     opened = set([0])   # boîte 0 déjà ouverte
-    keys = set(boxes[0])  # clés disponibles au départ
+    # clés disponibles au départ (protéger contre index error)
+    keys = set(boxes[0]) if boxes and boxes[0] else set()
 
     while keys:
         key = keys.pop()
 
-        if key < n and key not in opened:
+        if isinstance(key, int) and 0 <= key < n and key not in opened:
             opened.add(key)
             keys.update(boxes[key])
 

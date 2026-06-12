2897b44b41b3f60648d9a65cce27a4180674e46a
Rmxpvl <remy.pinville@gmail.com>
2026-06-12 23:49:10 +0200

    Add UTF-8 validation function to check valid encoding

---
+++ utf8_validation/0-validate_utf8.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+UTF-8 Validation
+"""
+
+
+def validUTF8(data):
+    """
+    Determines if a given data set represents a valid UTF-8 encoding.
+
+    Args:
+        data: List of integers
+
+    Returns:
+        True if data is a valid UTF-8 encoding, else False
+    """
+    remaining = 0
+
+    for num in data:
+        byte = num & 0xFF
+
+        if remaining == 0:
+            if byte >> 7 == 0:
+                continue
+            elif byte >> 5 == 0b110:
+                remaining = 1
+            elif byte >> 4 == 0b1110:
+                remaining = 2
+            elif byte >> 3 == 0b11110:
+                remaining = 3
+            else:
+                return False
+        else:
+            if byte >> 6 != 0b10:
+                return False
+            remaining -= 1
+
+    return remaining == 0
